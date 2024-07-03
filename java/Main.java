86b528f4e7fc7606e07095ac68d35538c8200435
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:10:16 +0900

    Add file

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		Printer printer = new Printer();
-	}
-}
-
-class Printer {
-	public void print() {
-		System.out.println("Hello, Java World!");
+		System.out.println("Hello, Main World!");
 	}
 }
