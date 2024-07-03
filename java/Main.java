51e765e2fe7a180bb29a8e21ab0629ca747084dd
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 12:16:17 +0900

    Update "switch" block

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		switch (answer) {
-			case 42:
+		switch (answer - 1) {
+			case 41:
 				System.out.println("The answer is 42.");
 				break;
 			default:
