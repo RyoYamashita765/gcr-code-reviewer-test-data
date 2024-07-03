ed4bf3c1f639ee874998f18c3c04242c38371758
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 12:15:59 +0900

    Add "switch" block

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
+		switch (answer) {
+			case 42:
+				System.out.println("The answer is 42.");
+				break;
+			default:
+				System.out.println("The answer is not 42.");
+				break;
+		}
 	}
 }
