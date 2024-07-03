6fce1da4ba55bb63597f09a3d68416a224265215
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 12:12:41 +0900

    Update "for" block

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			System.out.println("Hello, World! " + i);
 		}
 	}
