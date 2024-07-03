3e15febc18441fe2fbf0a6d5ed77b44045153b9b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:08:51 +0900

    Update one line to each method in multiple classes

--- java/Main.java
+++ java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Printer:");
 		Printer printer = new Printer();
 	}
 }
 
 class Printer {
 	public void print() {
-		System.out.println("Hello World!");
+		System.out.println("Hello, Java World!");
 		System.out.println("(from Printer)");
 	}
 }
