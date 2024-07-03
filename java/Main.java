a4dfb198167776899ba8178b96f75f2d86196e6c
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:01:15 +0900

    Add fields

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	final static String HELLO = "Hello";
+	static int count = 0;
+	double pi = 3.14;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
