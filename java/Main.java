98928852d0778464e50cd853251e4220e52750e4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:09:32 +0900

    Delete one line from each method in multiple classes

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Printer:");
 		Printer printer = new Printer();
 	}
 }
@@ -8,6 +7,5 @@ public class Main {
 class Printer {
 	public void print() {
 		System.out.println("Hello, Java World!");
-		System.out.println("(from Printer)");
 	}
 }
