4a3439a121314f25b424bfdb29e6cd9429d3b8a3
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 13:55:22 +0900

    Update "if" block

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Hello, World!");
+		if (args.length < 1) {
+			System.out.println("Hello, Java!");
 		} else {
 			System.out.println("Hello, " + args[0] + "!");
 		}
