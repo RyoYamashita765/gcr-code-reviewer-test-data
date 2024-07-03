e70ab369904c1e9565cbcb5dd0a2049a1650654f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:54:05 +0900

    Delete "while" block

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
-		int a = 10;
-		while (a >= 0) {
-			System.out.println("Hello World!");
-			a--;
-		}
 	}
 }
