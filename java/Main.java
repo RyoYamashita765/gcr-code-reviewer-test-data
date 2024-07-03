4471904500554a23a7e03a71ff9eb02e6e33acb0
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:52:10 +0900

    Add "while" block

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		int a = 10;
+		while (a > 0) {
+			System.out.println(a);
+			a--;
+		}
 	}
 }
