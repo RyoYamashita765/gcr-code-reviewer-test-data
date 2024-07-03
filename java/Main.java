7e326c6531c4424a67df3380e3e6e7abfd94eff1
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:24:13 +0900

    Add sample case: "Validate if variables are always safe to avoid Internal Server Error"

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		String result = "";
-		if (args.length == 0) {
-			result = "No arguments";
-		} else {
-			result = "One or more arguments";
-		}
-		System.out.println(result);
+		String a = null;
+		System.out.println(a.length());
+
+		int[] arr = { 1, 2, 3 };
+		System.out.println(arr[3]);
 	}
 }
