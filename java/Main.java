067fea2be79c50512840c8d7a0cd4f56ff1a2abe
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:39:51 +0900

    Add lines to some points

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
 
+
+
+		System.out.println("Hello World!");
+
+
+
+		System.out.println("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
 	}
 }
