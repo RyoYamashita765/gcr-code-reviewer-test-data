da1dfedf06d51f63d4ca4ce619bedb8451f71989
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:01:14 +0900

    Update "switch" block

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		switch (args.length) {
-		case 0:
+		switch (args.length + 1) {
+		case 1:
 			System.out.println("No arguments");
 			break;
 		default:
