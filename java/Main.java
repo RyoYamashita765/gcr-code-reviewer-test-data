1ee73419f2e971498b2b5bc52d4003afc15ab280
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:08:25 +0900

    Add one line to each method in multiple classes

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		Printer printer = new Printer();
 	}
 }
 
 class Printer {
 	public void print() {
 		System.out.println("Hello World!");
+		System.out.println("(from Printer)");
 	}
 }
