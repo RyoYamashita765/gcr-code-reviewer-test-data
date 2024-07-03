2d049db9d284dce65d4c0d96fd00abbd4941e521
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 12:20:10 +0900

    Update "while" block

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		while (answer > 0) {
+		while (answer >= 0) {
 			System.out.println("Hello, World!");
 			answer--;
 		}
