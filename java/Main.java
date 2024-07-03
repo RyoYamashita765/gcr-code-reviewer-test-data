4199f0e38aa361eed7a38e1efe32f066ab021dd0
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 13:57:06 +0900

    Delete "if" block

--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("Hello, Java!");
-		} else {
-			System.out.println("Hello, " + args[0] + "!");
-		}
+
 	}
 }
