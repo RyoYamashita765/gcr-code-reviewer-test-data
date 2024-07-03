7c107f09723344397a9f581e036b73a6235f29c4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:59:43 +0900

    Add method

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		printHello();
+	}
+
+	public static void printHello() {
+		System.out.println("Hello");
 	}
 }
