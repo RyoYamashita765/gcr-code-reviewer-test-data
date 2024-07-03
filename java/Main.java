3ab9f1b114fb7114a69598e0d0228ea0295aa43a
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 14:43:33 +0900

    Add "if" block

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		if (args.length > 0) {
+			System.out.println("Arguments:");
+			for (String arg : args) {
+				System.out.println(arg);
+			}
+		}
 	}
 }
