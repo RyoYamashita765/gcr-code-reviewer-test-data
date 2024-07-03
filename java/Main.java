50fdc0f2ce8be867bbd0215250007e9ee81951dd
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-03 15:18:46 +0900

    Add sample case: "Define an interface when introducing a new class."

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,49 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 	public static void main(String[] args) {
-		final double circleRadius = 5.0;
-		final double ciecleArea = 3.141592 * Math.pow(circleRadius, 2);
-		System.out.println("Circle area: " + ciecleArea);
+		DatabaseManager databaseManager = new DatabaseManager("jdbc:mysql://localhost:3306/test", "admin", "pass");
+		List<Map<String, String>> result = databaseManager.select("SELECT * FROM test_table");
+		result.forEach(System.out::println);
 	}
+}
+
+class DatabaseManager {
+	private String url;
+	private String username;
+	private String password;
+
+	public DatabaseManager(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+
+	public List<Map<String, String>> select(String query) {
+		List<Map<String, String>> output = new ArrayList<>();
+		try (Connection connection = DriverManager.getConnection(url, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(query)) {
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			while (resultSet.next()) {
+				Map<String, String> row = new HashMap<>();
+				for (int i = 1; i <= metaData.getColumnCount(); i++) {
+					row.put(metaData.getColumnName(i), resultSet.getString(i));
+				}
+				output.add(row);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return output;
+    }
 }
