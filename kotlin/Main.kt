6101cbd622b86e2b4be8e48a513e0c2aa258d6dd
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-11 18:39:43 +0900

    Add one line in "when" block

--- kotlin/Main.kt
+++ kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main() {
     val answer = 42
-    when (answer * 1) {
-        42 -> println("The answer is 42.")
+    when (answer - 1) {
+        42 -> {
+            println("The answer is 42.")
+            println(answer)
+        }
         else -> println("The answer is not 42.")
     }
 }
