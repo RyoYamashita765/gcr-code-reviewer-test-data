8657a1edbdd2e10431f1195da1d42bd373387b7d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-12 16:06:58 +0900

    Add sample case: "Write your code without else statements where possible."

--- kotlin/Main.kt
+++ kotlin/Main.kt
@@ -1,20 +1,29 @@
+class Customer(val isPremium: Boolean, val loyaltyYears: Int, val purchaseCount: Int)
+
+fun getDiscountPercentage(customer: Customer): Int {
+    if (customer.isPremium) {
+        if (customer.loyaltyYears > 5) {
+            return 20
+        } else {
+            return 15
+        }
+    } else {
+        if (customer.purchaseCount > 10) {
+            return 10
+        } else {
+            return 5
+        }
+    }
+}
+
 public class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            if (args.size > 0) {
-                if (args.size > 1) {
-                    if (args.size > 2) {
-                        println("Too many arguments")
-                    } else {
-                        println("Two arguments")
-                    }
-                } else {
-                    println("One argument")
-                }
-            } else {
-                println("No arguments")
-            }
+            val customer = Customer(true, 6, 10)
+            val discount = getDiscountPercentage(customer)
+            println("Customer: ${customer.isPremium}, ${customer.loyaltyYears}, ${customer.purchaseCount}")
+            println("Discount: $discount%")
         }
     }
 }
