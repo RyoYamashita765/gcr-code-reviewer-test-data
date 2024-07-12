14636be68fa9689aee43803bb8eb91b4f1023fcb
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-12 15:48:04 +0900

    Add sample case: "MUST avoid hard-coding."

--- kotlin/Main.kt
+++ kotlin/Main.kt
@@ -2,7 +2,9 @@ public class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
+            val circleRadius = 5.0
+            val circleArea = 3.141592 * circleRadius * circleRadius
+            println("Circle area is $circleArea")
         }
     }
 }
