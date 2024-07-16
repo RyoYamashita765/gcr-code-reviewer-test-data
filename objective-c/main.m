c9aae5871b8cd7d36f3f554302e96ace737e8b4e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-16 14:55:11 +0900

    Add interface and implementation

--- objective-c/main.m
+++ objective-c/main.m
@@ -1,8 +1,38 @@
 #import <Foundation/Foundation.h>
 
+@interface Person : NSObject {
+    NSString *name;
+    int age;
+}
+- (void)setName:(NSString *)name;
+- (void)setAge:(int)age;
+- (NSString *)getName;
+- (int)getAge;
+@end
+
+@implementation Person
+- (void)setName:(NSString *)newName {
+    self->name = newName;
+}
+- (void)setAge:(int)newAge {
+    self->age = newAge;
+}
+- (NSString *)getName {
+    return self->name;
+}
+- (int)getAge {
+    return self->age;
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello, World!");
+        Person *person = [[Person alloc] init];
+        [person setName:@"John"];
+        [person setAge:30];
+        NSLog(@"Name: %@", [person getName]);
+        NSLog(@"Age: %d", [person getAge]);
     }
     return 0;
 }
