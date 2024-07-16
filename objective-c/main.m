30ff802cb2e73255e691fa786a190c5f3babca90
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-16 17:05:38 +0900

    Delete "switch" block

--- objective-c/main.m
+++ objective-c/main.m
@@ -33,14 +33,6 @@ int main(int argc, const char * argv[]) {
         [person setAge:30];
         NSLog(@"Name: %@", [person getName]);
         NSLog(@"Age: %d", [person getAge]);
-        switch ([person getAge] * 1) {
-            case 30:
-                NSLog(@"Age is 30");
-                break;
-            default:
-                NSLog(@"Age is not 30");
-                break;
-        }
     }
     return 0;
 }
