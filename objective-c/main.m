583623c92f23b6aea6b6fa87c62110115a665d1f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-16 18:50:50 +0900

    Add multiple lines in "switch" block

--- objective-c/main.m
+++ objective-c/main.m
@@ -35,6 +35,9 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Age: %d", [person getAge]);
         switch ([person getAge] + 1) {
             case 31:
+                NSLog(@"=====================");
+                NSLog(@"======= Result ======");
+                NSLog(@"=====================");
                 NSLog(@"Age is 30");
                 break;
             default:
