f629f999aa9beda544a6a2b1d4597fe8601ef57d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 16:12:24 +0900

    Addd class

--- objective-c/main.m
+++ objective-c/main.m
@@ -1,8 +1,15 @@
 #import <Foundation/Foundation.h>
 
+@interface Car : NSObject
+@end
+
+@implementation Car;
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
+        Car *car = [[Car alloc] init];
+        NSLog(@"Hello, World!");
     }
     return 0;
 }
