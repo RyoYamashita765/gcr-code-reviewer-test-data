5e84b67b56dfa180a0a3a8353956785573b1db42
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 10:42:53 +0900

    Delete lines in "if" block

--- objective-c/main.m
+++ objective-c/main.m
@@ -34,9 +34,7 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Name: %@", [person getName]);
         NSLog(@"Age: %d", [person getAge]);
         if ([person getAge] >= 18) {
-            NSLog(@"---------------------");
             NSLog(@"Type: Adult");
-            NSLog(@"---------------------");
         } else {
             NSLog(@"Type is Child");
         }
