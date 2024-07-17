05e68009d0df11dc3270de32e0056a98159fbe22
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 11:54:54 +0900

    Add multiple lines to some points

--- objective-c/main.m
+++ objective-c/main.m
@@ -27,12 +27,16 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSLog(@"Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+        NSLog(@"Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
         NSLog(@"Hello, World!");
         Person *person = [[Person alloc] init];
         [person setName:@"John"];
         [person setAge:30];
         NSLog(@"Name: %@", [person getName]);
         NSLog(@"Age: %d", [person getAge]);
+        NSLog(@"Goodbye, World!");
+        NSLog(@"Duis aute irure dolor in reprehenderit in voluptate ...");
     }
     return 0;
 }
