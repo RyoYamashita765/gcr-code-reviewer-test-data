88f41c0382ee30e7d005fc3c133553e4455f838f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 12:05:15 +0900

    Delete multiple lines to some points

--- objective-c/main.m
+++ objective-c/main.m
@@ -27,16 +27,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Deserunt mollit anim id est laborum.");
-        NSLog(@"Sed ut perspiciatis unde omnis iste natus...");
-        NSLog(@"Hello, World!");
         Person *person = [[Person alloc] init];
         [person setName:@"John"];
         [person setAge:30];
-        NSLog(@"Name: %@", [person getName]);
-        NSLog(@"Age: %d", [person getAge]);
-        NSLog(@"Ut enim ad minima veniam, quis nostrum exercitationem ...");
-        NSLog(@"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...");
     }
     return 0;
 }
