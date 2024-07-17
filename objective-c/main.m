6a7746ac329ee0f37e458f0670afb3f1965f53d3
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 16:17:59 +0900

    Add class with method

--- objective-c/main.m
+++ objective-c/main.m
@@ -14,6 +14,16 @@
 @implementation Bicycle;
 @end
 
+@interface Motorcycle : NSObject
+@end
+
+@implementation Motorcycle;
+- (void)startEngine {
+    NSLog(@"Vroom!");
+}
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Car *car = [[Car alloc] init];
