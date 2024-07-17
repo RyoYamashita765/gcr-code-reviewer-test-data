a74584bee7b1cc0a3be720e35ad3ceb66e941b76
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 16:33:48 +0900

    Add class

--- objective-c/main.m
+++ objective-c/main.m
@@ -6,10 +6,34 @@
 @implementation Car;
 @end
 
+@interface Bicycle : NSObject;
+- (void) startEngine;
+@end
+
+@implementation Bicycle;
+- (void) startEngine {
+    NSLog(@"Go!");
+}
+@end
+
+@interface Motorcycle : NSObject;
+- (void) startEngine;
+@end
+
+@implementation Motorcycle;
+- (void) startEngine {
+    NSLog(@"Vroom!");
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Car *car = [[Car alloc] init];
         NSLog(@"Hello, World!");
+        Bicycle *bicycle = [[Bicycle alloc] init];
+        [bicycle startEngine];
+        Motorcycle *motorcycle = [[Motorcycle alloc] init];
+        [motorcycle startEngine];
     }
     return 0;
 }
