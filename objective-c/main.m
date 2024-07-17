6f69294c2597d87d267440436dc5964d903cec18
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 10:55:04 +0900

    Update "switch" block

--- objective-c/main.m
+++ objective-c/main.m
@@ -33,11 +33,11 @@ int main(int argc, const char * argv[]) {
         [person setAge:30];
         NSLog(@"Name: %@", [person getName]);
         NSLog(@"Age: %d", [person getAge]);
-        switch ([person getAge]) {
+        switch ([person getAge] + 1) {
             case 30:
                 NSLog(@"Age is 30");
                 break;
-            case 31:
+            case 32:
                 NSLog(@"Age is 31");
                 break;
             default:
