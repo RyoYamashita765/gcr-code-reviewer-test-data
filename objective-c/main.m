df96da2f0a2a81142c2209b17f050e77c43afa19
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-17 15:32:24 +0900

    Add class

--- objective-c/main.m
+++ objective-c/main.m
@@ -28,11 +28,33 @@
 }
 @end
 
+@interface Student : Person {
+    NSString *school;
+}
+- (void)setSchool:(NSString *)newSchool;
+- (NSString *)getSchool;
+@end
+
+@implementation Student
+- (void)setSchool:(NSString *)newSchool {
+    self->school = newSchool;
+}
+- (NSString *)getSchool {
+    return self->school;
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Person *person = [[Person alloc] init];
         [person setName:@"John"];
         [person setAge:20];
+
+        Student *student = [[Student alloc] init];
+        [student setName:@"Jane"];
+        [student setAge:21];
+        [student setSchool:@"University of FooBar"];
+        NSLog(@"Name: %@, Age: %d, School: %@", [student getName], [student getAge], [student getSchool]);
     }
     return 0;
 }
