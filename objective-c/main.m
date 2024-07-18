9d2ed6d180d85075813f87ff0c9a40f4265cc7f9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 11:47:41 +0900

    Add sample case: "Validate if variables are always safe to avoid Internal Server Error"

--- objective-c/main.m
+++ objective-c/main.m
@@ -2,16 +2,12 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *result;
+        NSString *str1 = nil;
+        NSLog(@"The length of str1 is %lu", (unsigned long)[str1 length]);
         
-        if (argc <= 1) {
-            result = @"Hello, World!";
-        } else {
-            NSString *name = [NSString stringWithUTF8String:argv[1]];
-            result = [NSString stringWithFormat:@"Hello, %@!", name];
-        }
-        
-        NSLog(@"%@", result);
+        NSArray *arr = @[@1, @2, @3];
+        id element = arr[4];
+        NSLog(@"Element at index 4: %@", element);
     }
     return 0;
 }
