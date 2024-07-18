a63c8b160cb23f6795ecbc54d417ff9d36baa92e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 17:38:45 +0900

    Update one line to some points

--- swift/main.swift
+++ swift/main.swift
@@ -2,11 +2,11 @@ import Foundation
 
 print("Programming Test")
 print("By Swift language")
-print("--------------------")
+print("====================")
 print("Hello, Swift World!")
 print("Hello, Swift!!!")
 print("Goodbye......!")
 print("Goodbye, World!")
 print("--------------------")
 print("End of Programming Test")
-print("--------------------")
+print("====================")
