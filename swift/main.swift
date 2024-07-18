07ba1d8d0d2d215764417fbe39a31f759676b166
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 18:01:28 +0900

    Add "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,3 +5,11 @@ print("Hello, Swift World!")
 print("Hello, Swift!!!")
 print("Goodbye......!")
 print("Goodbye, World!")
+var a = 10
+var b = 20
+var c = a + b
+if c > 20 {
+    print("Sum is greater than 20")
+} else {
+    print("Sum is less than 20")
+}
