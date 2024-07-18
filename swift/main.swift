310ba06fd3f4fe3d8f8271509de32c18f0e2fdb5
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 17:44:08 +0900

    Delete multiple lines

--- swift/main.swift
+++ swift/main.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 print("Laugnage Programming Test")
-print("for Swift language")
-print("====================")
 print("Hello, Swift World!")
 print("Hello, Swift!!!")
 print("Goodbye......!")
