0995545fe9961642346dea9f0b9c27f315bac7a9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 11:52:00 +0900

    Add sample case: "#import statements should be in alphabetical order."

--- objective-c/main.m
+++ objective-c/main.m
@@ -1,52 +1,13 @@
+#import <BetaLibrary.h>
 #import <Foundation/Foundation.h>
-
-@interface Customer : NSObject
-
-@property (nonatomic, assign) BOOL isPremium;
-@property (nonatomic, assign) NSInteger loyaltyYears;
-@property (nonatomic, assign) NSInteger purchaseCount;
-
-- (instancetype)initWithIsPremium:(BOOL)isPremium loyaltyYears:(NSInteger)loyaltyYears purchaseCount:(NSInteger)purchaseCount;
-
-@end
-
-@implementation Customer
-
-- (instancetype)initWithIsPremium:(BOOL)isPremium loyaltyYears:(NSInteger)loyaltyYears purchaseCount:(NSInteger)purchaseCount {
-    self = [super init];
-    if (self) {
-        _isPremium = isPremium;
-        _loyaltyYears = loyaltyYears;
-        _purchaseCount = purchaseCount;
-    }
-    return self;
-}
-
-@end
-
-NSInteger getDiscountPercentage(Customer *customer) {
-    if (customer.isPremium) {
-        if (customer.loyaltyYears > 5) {
-            return 20;
-        } else {
-            return 15;
-        }
-    } else {
-        if (customer.purchaseCount > 10) {
-            return 10;
-        } else {
-            return 5;
-        }
-    }
-}
+#import <EpsilonLibrary.h>
+#import <GammaLibrary.h>
+#import <AlphaLibrary.h>
+#import <DeltaLibrary.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Customer *customer = [[Customer alloc] initWithIsPremium:YES loyaltyYears:6 purchaseCount:10];
-        NSInteger discount = getDiscountPercentage(customer);
-        
-        NSLog(@"Customer: %@, %ld, %ld", customer.isPremium ? @"YES" : @"NO", (long)customer.loyaltyYears, (long)customer.purchaseCount);
-        NSLog(@"Discount: %ld%%", (long)discount);
+        NSLog(@"Hello, World!");
     }
     return 0;
 }
