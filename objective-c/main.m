384be22e41a30a091fd17eb05cf02a7f8b980028
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 11:50:27 +0900

    Add sample case: "Write your code without else statements where possible."

--- objective-c/main.m
+++ objective-c/main.m
@@ -1,22 +1,52 @@
 #import <Foundation/Foundation.h>
 
+@interface Customer : NSObject
+
+@property (nonatomic, assign) BOOL isPremium;
+@property (nonatomic, assign) NSInteger loyaltyYears;
+@property (nonatomic, assign) NSInteger purchaseCount;
+
+- (instancetype)initWithIsPremium:(BOOL)isPremium loyaltyYears:(NSInteger)loyaltyYears purchaseCount:(NSInteger)purchaseCount;
+
+@end
+
+@implementation Customer
+
+- (instancetype)initWithIsPremium:(BOOL)isPremium loyaltyYears:(NSInteger)loyaltyYears purchaseCount:(NSInteger)purchaseCount {
+    self = [super init];
+    if (self) {
+        _isPremium = isPremium;
+        _loyaltyYears = loyaltyYears;
+        _purchaseCount = purchaseCount;
+    }
+    return self;
+}
+
+@end
+
+NSInteger getDiscountPercentage(Customer *customer) {
+    if (customer.isPremium) {
+        if (customer.loyaltyYears > 5) {
+            return 20;
+        } else {
+            return 15;
+        }
+    } else {
+        if (customer.purchaseCount > 10) {
+            return 10;
+        } else {
+            return 5;
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int argCount = argc - 1;
-
-        if (argCount > 0) {
-            if (argCount > 1) {
-                if (argCount > 2) {
-                    NSLog(@"Too many arguments");
-                } else {
-                    NSLog(@"Two arguments");
-                }
-            } else {
-                NSLog(@"One argument");
-            }
-        } else {
-            NSLog(@"No arguments");
-        }
+        Customer *customer = [[Customer alloc] initWithIsPremium:YES loyaltyYears:6 purchaseCount:10];
+        NSInteger discount = getDiscountPercentage(customer);
+        
+        NSLog(@"Customer: %@, %ld, %ld", customer.isPremium ? @"YES" : @"NO", (long)customer.loyaltyYears, (long)customer.purchaseCount);
+        NSLog(@"Discount: %ld%%", (long)discount);
     }
     return 0;
 }
