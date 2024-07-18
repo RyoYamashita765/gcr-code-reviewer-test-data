828d4555cb4ddb992f9a6541f1e901fc3ce2de70
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-18 11:46:02 +0900

    Add sample case: "Use ternary operator when the logic has a single if-else statement."

--- objective-c/main.m
+++ objective-c/main.m
@@ -1,90 +1,17 @@
 #import <Foundation/Foundation.h>
 
-@interface User : NSObject
-@property (nonatomic, strong) NSString *username;
-@property (nonatomic, strong) NSString *email;
-
-- (instancetype)initWithUsername:(NSString *)username email:(NSString *)email;
-@end
-
-@implementation User
-- (instancetype)initWithUsername:(NSString *)username email:(NSString *)email {
-    self = [super init];
-    if (self) {
-        _username = username;
-        _email = email;
-    }
-    return self;
-}
-@end
-
-@interface UserRepository : NSObject
-- (BOOL)isEmailTaken:(NSString *)email;
-- (void)saveUser:(User *)user;
-@end
-
-@implementation UserRepository
-- (BOOL)isEmailTaken:(NSString *)email {
-    // Assume SampleDatabase is a class with class methods
-    User *user = [SampleDatabase findUserByEmail:email];
-    return user != nil;
-}
-
-- (void)saveUser:(User *)user {
-    [SampleDatabase saveUser:user];
-}
-@end
-
-@interface EmailService : NSObject
-- (void)sendWelcomeEmail:(NSString *)email;
-@end
-
-@implementation EmailService
-- (void)sendWelcomeEmail:(NSString *)email {
-    // Assume SampleClient is a class with class methods
-    [SampleClient sendEmail:email withMessage:@"Welcome to our service!"];
-}
-@end
-
-@interface UserService : NSObject
-@property (nonatomic, strong) UserRepository *userRepository;
-@property (nonatomic, strong) EmailService *emailService;
-@end
-
-@implementation UserService
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _userRepository = [[UserRepository alloc] init];
-        _emailService = [[EmailService alloc] init];
-    }
-    return self;
-}
-
-- (void)registerUserWithUsername:(NSString *)username email:(NSString *)email {
-    if ([self.userRepository isEmailTaken:email]) {
-        @throw [NSException exceptionWithName:NSInvalidArgumentException
-                                       reason:@"Email is already registered"
-                                     userInfo:nil];
-    }
-    
-    User *user = [[User alloc] initWithUsername:username email:email];
-    [self.userRepository saveUser:user];
-    [self.emailService sendWelcomeEmail:email];
-}
-
-@end
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        UserService *userService = [[UserService alloc] init];
-        @try {
-            [userService registerUserWithUsername:@"johndoe" email:@"john@example.com"];
-            NSLog(@"User registered successfully");
-        } @catch (NSException *exception) {
-            NSLog(@"Failed to register user: %@", exception.reason);
+        NSString *result;
+        
+        if (argc <= 1) {
+            result = @"Hello, World!";
+        } else {
+            NSString *name = [NSString stringWithUTF8String:argv[1]];
+            result = [NSString stringWithFormat:@"Hello, %@!", name];
         }
+        
+        NSLog(@"%@", result);
     }
     return 0;
 }
