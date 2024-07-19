06d113e32b8e67519a7e13ead56c910aa18d6d8e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:03:04 +0900

    Delete "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -1,11 +1,3 @@
 import Foundation
 
 print("Hello, World!")
-
-for i in 1..<30 {
-    print("--------------------------------")
-    print("[[i = \(i)]]")
-    print("  i - 1 = \(i - 1)")
-    print("  i / 3 = \(i / 3)")
-    print("  i * 3 = \(i * 3)")
-}
