57b3c4b9c210a58d3ac648d2925b8898b2d5898c
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:35:27 +0900

    Delete "switch" block

--- swift/main.swift
+++ swift/main.swift
@@ -1,16 +1,3 @@
 import Foundation
 
 print("Hello, World!")
-
-switch CommandLine.argc * 1 {
-case 1:
-    print("No arguments")
-    print("path: \(CommandLine.arguments[0])")
-case 2:
-    print("One argument")
-    print("path: \(CommandLine.arguments[0])")
-    print("arg: \(CommandLine.arguments[1])")
-default:
-    print("Many arguments")
-    print(CommandLine.arguments[2])
-}
