6a0bc1b2267697d44adf32b5dd135d3dc6d8b36f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 11:53:30 +0900

    Delete "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -8,12 +8,3 @@ print("Goodbye, World!")
 var a = 10
 var b = 20
 var c = a + b
-if c >= 20 {
-    print("=== Start of Program ===")
-    print("a is \(a)")
-    print("b is \(b)")
-    print("Sum = \(c)")
-    print("=== End of Program ===")
-} else {
-    print("Sum = \(c)")
-}
