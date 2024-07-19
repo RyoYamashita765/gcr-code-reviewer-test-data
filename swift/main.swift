5447744546226b7ca28579ddf80ff781afe68b3a
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 10:51:04 +0900

    Add multiple lines to some points in "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -11,10 +11,14 @@ var c = a + b
 if c > 20 {
     print("== Start of Program ==")
     print("Sum is greater than 20")
+    print("a is \(a)")
+    print("b is \(b)")
     print("Sum is \(c)")
     print("======================")
     print("=== End of Program ===")
 } else {
     print("Sum is less than 20")
+    print("a is \(a)")
+    print("b is \(b)")
     print("Sum is \(c)")
 }
