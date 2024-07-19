7f6c069295a07fdf098b3beb120471c3f28b98e9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 11:52:12 +0900

    Delete one line from some points in "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -10,12 +10,10 @@ var b = 20
 var c = a + b
 if c >= 20 {
     print("=== Start of Program ===")
-    print("Sum is greater than 20")
     print("a is \(a)")
     print("b is \(b)")
     print("Sum = \(c)")
     print("=== End of Program ===")
 } else {
-    print("Sum is less than 20")
     print("Sum = \(c)")
 }
