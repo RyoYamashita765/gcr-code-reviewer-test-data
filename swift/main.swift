710dede996580a4be35fe079277dee820a312233
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:30:59 +0900

    Update multiple lines to some points in "switch" block

--- swift/main.swift
+++ swift/main.swift
@@ -4,13 +4,13 @@ print("Hello, World!")
 
 switch CommandLine.argc * 1 {
 case 1:
-    print("No argument")
-    print(CommandLine.arguments[0])
+    print("No arguments")
+    print("path: \(CommandLine.arguments[0])")
 case 2:
     print("One argument")
     print(CommandLine.argc)
-    print(CommandLine.arguments[0])
-    print(CommandLine.arguments[1])
+    print("path: \(CommandLine.arguments[0])")
+    print("arg: \(CommandLine.arguments[1])")
     print(CommandLine.arguments)
 default:
     print("Many arguments")
