7627f461e9f1a4664417317606c515a0aaf88129
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:02:37 +0900

    Delete one line from some points in "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,9 +5,7 @@ print("Hello, World!")
 for i in 1..<30 {
     print("--------------------------------")
     print("[[i = \(i)]]")
-    print("  i + 1 = \(i + 1)")
     print("  i - 1 = \(i - 1)")
     print("  i / 3 = \(i / 3)")
     print("  i * 3 = \(i * 3)")
-    print("end")
 }
