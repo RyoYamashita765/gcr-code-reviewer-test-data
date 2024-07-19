c2cd0da572b1cfffaaac307e751a4e4106cc1558
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 15:26:57 +0900

    Add multiple lines to some points in "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -6,7 +6,11 @@ for i in 0..<10 {
     print("===========")
     print("[i = \(i)]")
     print("  i + 1 = \(i + 1)")
+    print("  i - 1 = \(i - 1)")
+    print("  i / 2 = \(i / 2)")
     print("  i * 2 = \(i * 2)")
     print("  i * i = \(i * i)")
+    print("  i % 2 = \(i % 2)")
+    print("  i % 3 = \(i % 3)")
     print("===========")
 }
