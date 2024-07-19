d81de3bcc216c31844875e3685d78021079bc7a4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 15:24:33 +0900

    Add one line to some points in "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,6 +5,8 @@ print("Hello, World!")
 for i in 0..<10 {
     print("===========")
     print("[i = \(i)]")
+    print("  i + 1 = \(i + 1)")
     print("  i * 2 = \(i * 2)")
     print("  i * i = \(i * i)")
+    print("===========")
 }
