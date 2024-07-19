3cd61d408356138656ddb5b481a7311712b198a7
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 15:57:59 +0900

    Update multiple lines to some points in "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -3,14 +3,14 @@ import Foundation
 print("Hello, World!")
 
 for i in 1..<30 {
-    print("==========")
-    print("[i = \(i)]")
+    print("--------------------------------")
+    print("[[i = \(i)]]")
     print("  i + 1 = \(i + 1)")
     print("  i - 1 = \(i - 1)")
     print("  i / 3 = \(i / 3)")
     print("  i * 3 = \(i * 3)")
     print("  i * i = \(i * i)")
     print("  i % 2 = \(i % 2)")
-    print("  i % 3 = \(i % 3)")
-    print("")
+    print("  i %  5= \(i % 5)")
+    print("end")
 }
