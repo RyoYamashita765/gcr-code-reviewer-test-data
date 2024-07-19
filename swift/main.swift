034994c9f0a24f17f994d2e3ab31285c41e1226e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 15:28:35 +0900

    Update one line in "for" block (2)

--- swift/main.swift
+++ swift/main.swift
@@ -8,7 +8,7 @@ for i in 0..<20 {
     print("  i + 1 = \(i + 1)")
     print("  i - 1 = \(i - 1)")
     print("  i / 2 = \(i / 2)")
-    print("  i * 2 = \(i * 2)")
+    print("  i * 3 = \(i * 3)")
     print("  i * i = \(i * i)")
     print("  i % 2 = \(i % 2)")
     print("  i % 3 = \(i % 3)")
