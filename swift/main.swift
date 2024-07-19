a31d726135a40bcb6e96abf4d24794d2890c3a21
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:24:50 +0900

    Add one line to some points in "switch" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,9 +5,11 @@ print("Hello, World!")
 switch CommandLine.argc {
 case 1:
     print("No arguments")
+    print(CommandLine.arguments[0])
 case 2:
     print("One argument")
     print(CommandLine.arguments[1])
+    print(CommandLine.arguments[1].uppercased())
 default:
     print("Many arguments")
     print(CommandLine.argc)
