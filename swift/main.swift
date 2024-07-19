62fa04ff23c29b4089e340deeaa7ded51a696258
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:22:32 +0900

    Add "switch" block

--- swift/main.swift
+++ swift/main.swift
@@ -1,3 +1,12 @@
 import Foundation
 
 print("Hello, World!")
+
+switch CommandLine.argc {
+case 1:
+    print("No arguments")
+case 2:
+    print("One argument")
+default:
+    print("Many arguments")
+}
