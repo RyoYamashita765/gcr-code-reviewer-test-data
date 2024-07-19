d4a05e2d8877b3d3597fdc588f76e1ba1b1b4279
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-19 16:25:37 +0900

    Add multiple lines to some points in "switch" block

--- swift/main.swift
+++ swift/main.swift
@@ -8,10 +8,14 @@ case 1:
     print(CommandLine.arguments[0])
 case 2:
     print("One argument")
+    print(CommandLine.argc)
+    print(CommandLine.arguments)
     print(CommandLine.arguments[1])
     print(CommandLine.arguments[1].uppercased())
 default:
     print("Many arguments")
     print(CommandLine.argc)
     print(CommandLine.arguments)
+    print(CommandLine.arguments[0])
+    print(CommandLine.arguments[1])
 }
