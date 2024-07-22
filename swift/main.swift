fe755e204969056d7150e8f7ea8ff425bcb7d103
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 10:38:24 +0900

    Delete one line from some points in "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -7,9 +7,7 @@ while a <= 30 {
     print("[a = \(a)]")
     print("  a + 2 = \(a + 2)")
     print("  a + 4 = \(a + 4)")
-    print("  a + 6 = \(a + 6)")
     print("  a + a = \(a + a)")
-    print("  a + a + a = \(a + a + a)")
     print("  a * a = \(a * a)")
     print("")
     a += 1
