9d2e087273d9fb719b195bdd6a582938e1a0fee4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 14:32:39 +0900

    Add class

--- swift/main.swift
+++ swift/main.swift
@@ -1,3 +1,15 @@
 import Foundation
 
+class Person {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
 print("Hello, World!")
+let person = Person(name: "John", age: 30)
+print("Person: \(person.name), \(person.age)")
