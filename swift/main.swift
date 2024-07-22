c7e7c886c7b14a93d1eaa469ae064d73b23fa246
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 10:27:44 +0900

    Update multiple lines to some points in "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -6,13 +6,13 @@ var a = 1
 while a <= 30 {
     print("[a = \(a)]")
     print("  a + 2 = \(a + 2)")
-    print("  a + 3 = \(a + 3)")
-    print("  a + 5 = \(a + 5)")
+    print("  a + 4 = \(a + 4)")
+    print("  a + 6 = \(a + 6)")
     print("  a + a = \(a + a)")
     print("  a + a + a = \(a + a + a)")
     print("  a * a = \(a * a)")
-    print("  a * a + 2 = \(a * a + 2)")
-    print("  a * a - a = \(a * a - a)")
+    print("  a * a + 3 = \(a * a + 3)")
+    print("  a * a - 4 = \(a * a - 4)")
     print("  a * a * a = \(a * a * a)")
     print("")
     a += 1
