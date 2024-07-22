ec0a4658119da8213934c5b83d5f954d2092b59d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 10:20:34 +0900

    Add multiple lines in "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,6 +5,8 @@ print("Hello, World!")
 var a = 1
 while a < 10 {
     print("a = \(a)")
+    print("  a + a = \(a + a)")
+    print("  a + a + a = \(a + a + a)")
     print("  a * a = \(a * a)")
     print("  a * a * a = \(a * a * a)")
     a += 1
