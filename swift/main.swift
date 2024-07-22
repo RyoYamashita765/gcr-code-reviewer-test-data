3d5ffd4c4b95ac87d219e66fae93f15c01701a5e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 10:39:02 +0900

    Delete "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -1,14 +1,3 @@
 import Foundation
 
 print("Hello, World!")
-
-var a = 1
-while a <= 30 {
-    print("[a = \(a)]")
-    print("  a + 2 = \(a + 2)")
-    print("  a + 4 = \(a + 4)")
-    print("  a + a = \(a + a)")
-    print("  a * a = \(a * a)")
-    print("")
-    a += 1
-}
