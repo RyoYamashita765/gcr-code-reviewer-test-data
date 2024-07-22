068849d9e32fc62a45e005792682f377cee066a9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-22 16:20:26 +0900

    Add sample case: "MUST avoid hard-coding."

--- swift/main.swift
+++ swift/main.swift
@@ -1,21 +1,6 @@
 import Foundation
 
-class Person {
-    var name: String
-    var age: Int
-
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
-
-    func sayHello() {
-        print("Hello, \(name)!")
-    }
-}
-
-let logger = ConsoleLogger()
-logger.info(message: "Hello, World!")
-let person = Person(name: "John", age: 30)
-logger.info(message: "Person: \(person.name), \(person.age)")
-person.sayHello()
+print("Hello, World!")
+let radius = 5.0
+let area = 3.141592 * radius * radius
+print("The area: \(area)")
