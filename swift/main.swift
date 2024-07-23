1a785a5e98377bb789493484f8fbb86112b683e3
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-23 11:08:20 +0900

    Add sample case: "Define an interface when introducing a new class."

--- swift/main.swift
+++ swift/main.swift
@@ -1,6 +1,25 @@
 import Foundation
 
-print("Hello, World!")
-let radius = 5.0
-let area = 3.141592 * radius * radius
-print("The area: \(area)")
+class Car {
+    var brand: String
+    var model: String
+    var year: Int
+    
+    init(brand: String, model: String, year: Int) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+    
+    func startEngine() {
+        print("The \(brand) \(model) engine is starting...")
+    }
+    
+    func stopEngine() {
+        print("The \(brand) \(model) engine is stopping...")
+    }
+}
+
+let myCar = Car(brand: "FooBar", model: "HogePiyo", year: 2022)
+myCar.startEngine()
+myCar.stopEngine()
