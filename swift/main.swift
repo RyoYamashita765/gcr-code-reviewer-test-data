ecbf2865016648a1cf713a761ab4cbc84a924a50
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:55:48 +0900

    Add class from protocol

--- swift/main.swift
+++ swift/main.swift
@@ -15,10 +15,33 @@ class Person {
     }
 }
 
+protocol Logger {
+    func info(message: String)
+    func warning(message: String)
+    func error(message: String)
+}
+
+class ConsoleLogger: Logger {
+    func info(message: String) {
+        print("INFO: \(message)")
+    }
+
+    func warning(message: String) {
+        print("WARNING: \(message)")
+    }
+
+    func error(message: String) {
+        print("ERROR: \(message)")
+    }
+}
+
 func main(){
+    let logger = ConsoleLogger()
+    logger.info(message: "Hello, World!")
     let person = Person(name: "John", age: 30)
     print("Person: \(person.name), \(person.age)")
     person.sayHello()
+    logger.warning(message: "This is a warning message.")
 }
 
 main()
