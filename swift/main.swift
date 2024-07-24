d96391c22cb29b1deafae72c56d4aa36718d2943
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 16:31:53 +0900

    Delete "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -2,11 +2,6 @@ import Foundation
 
 func main(){
     let arguments = CommandLine.arguments
-    for i in 0..<arguments.count {
-        print("--")
-        print("--- Arguments ---")
-        print(arguments[i])
-    }
 }
 
 main()
