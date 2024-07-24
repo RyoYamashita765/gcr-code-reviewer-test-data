086f64e173f90570140da7a1dc989067c96d947c
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:36:28 +0900

    Add multiple lines to some points in "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -5,9 +5,13 @@ func main(){
     while a < 10 {
         print("a = \(a)")
         print("  a + 1 = \(a + 1)")
+        print("  a + 2 = \(a + 2)")
+        print("  a + 3 = \(a + 3)")
         print("  a + a = \(a + a)")
         print("  a + a + a = \(a + a + a)")
         print("  a * a = \(a * a)")
+        print("  a * a + a = \(a * a + a)")
+        print("  a * a - a = \(a * a - a)")
         print("  a * a * a = \(a * a * a)")
         print("")
         a += 1
