1e187a8f0e1f3614c9d71ddbb9515de535311b90
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 15:09:57 +0900

    Update multiple lines to some points in "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -6,13 +6,13 @@ func main(){
         print("--- Command Line Arguments ---")
         print("Argument's count: \(arguments.count)")
         print("Hello, \(arguments[1])!")
-        print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-        print("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+        print("Lorem ipsum dolor sit amet,")
+        print("consectetur adipiscing elit.")
     } else {
         print("=== No Arguments ===")
         print("Hello, Swift World!")
-        print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-        print("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+        print("Lorem ipsum dolor sit amet,")
+        print("consectetur adipiscing elit.")
         print("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris")
         print("======================")
     }
