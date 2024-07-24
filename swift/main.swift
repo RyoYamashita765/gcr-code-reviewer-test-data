a9156bc66c485d02118b373f38a3c20638780bb9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:54:28 +0900

    Add method in class

--- swift/main.swift
+++ swift/main.swift
@@ -9,11 +9,16 @@ class Person {
         self.name = name
         self.age = age
     }
+
+    func sayHello() {
+        print("Hello, \(name)!")
+    }
 }
 
 func main(){
     let person = Person(name: "John", age: 30)
     print("Person: \(person.name), \(person.age)")
+    person.sayHello()
 }
 
 main()
