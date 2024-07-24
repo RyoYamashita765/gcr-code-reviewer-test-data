e4f80816bcf8647fa53e95eaee86354bd48e8af6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 16:28:56 +0900

    Update multiple lines in "for" block (2)

--- swift/main.swift
+++ swift/main.swift
@@ -2,13 +2,13 @@ import Foundation
 
 func main(){
     let arguments = CommandLine.arguments
-    for i in 1..<arguments.count {
+    for i in 0..<arguments.count {
         print("-")
         print("--- Arguments ---")
         print(arguments[i])
         print(arguments[i].count)
         print("--------------------")
-        print("")
+        print("-")
     }
 }
 
