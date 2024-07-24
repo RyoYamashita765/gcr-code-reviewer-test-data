cd74d2849e737ce1fb78f3525d8c4fba214aff41
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 15:07:27 +0900

    Update multiple lines in "if" block (2)

--- swift/main.swift
+++ swift/main.swift
@@ -2,14 +2,14 @@ import Foundation
 
 func main(){
     let arguments = CommandLine.arguments
-    if arguments.count >= 1 {
+    if arguments.count > 0 {
         print("--- Command Line Arguments ---")
         print("Arguments: \(arguments.count)")
         print("Hello, \(arguments[1])!")
         print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
         print("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
     } else {
-        print("=== Default Output ===")
+        print("=== No Arguments ===")
         print("Hello, World!")
         print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
         print("Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
