cbe9b5f9d968c5d0f592210d2ea4bbba56d79803
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 18:01:48 +0900

    Delete class

--- swift/main.swift
+++ swift/main.swift
@@ -15,33 +15,10 @@ class Person {
     }
 }
 
-class ConsoleLogger {
-    var logLevel: Int = 0
-
-    func info(message: String) {
-        print("INFO: \(message)")
-    }
-
-    func warning(message: String) {
-        print("WARNING: \(message)")
-    }
-
-    func error(message: String) {
-        print("ERROR: \(message)")
-    }
-
-    func debug(message: String) {
-        print("DEBUG: \(message)")
-    }
-}
-
 func main(){
-    let logger = ConsoleLogger()
-    logger.info(message: "Hello, World!")
     let person = Person(name: "John", age: 30)
     print("Person: \(person.name), \(person.age)")
     person.sayHello()
-    logger.warning(message: "This is a warning message.")
 }
 
 main()
