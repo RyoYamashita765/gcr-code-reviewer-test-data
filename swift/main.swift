38eb1471de3f014d32b664e3eee90c985c41714d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 16:27:35 +0900

    Update one line in "for" block (2)

--- swift/main.swift
+++ swift/main.swift
@@ -4,7 +4,7 @@ func main(){
     let arguments = CommandLine.arguments
     for i in 0..<arguments.count {
         print("")
-        print("--------------------")
+        print("--- Arguments ---")
         print(arguments[i])
         print(arguments[i].count)
         print("--------------------")
