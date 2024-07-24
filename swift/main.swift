43782bffba9b7f768d9178f5855ef1449948d4f3
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 15:32:38 +0900

    Add one line to some points in "for" block

--- swift/main.swift
+++ swift/main.swift
@@ -3,8 +3,10 @@ import Foundation
 func main(){
     let arguments = CommandLine.arguments
     for argument in arguments {
+        print("")
         print("--------------------")
         print(argument)
+        print(argument.count)
         print("--------------------")
         print("")
     }
