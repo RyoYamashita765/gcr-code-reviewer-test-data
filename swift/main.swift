d689adea29fcd7bdd7e1550e9adca162881939ba
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 14:40:02 +0900

    Delete multiple lines to some points

--- swift/main.swift
+++ swift/main.swift
@@ -1,13 +1,7 @@
 import Foundation
 
 func main(){
-    print("Laugnage Programming Test")
-    print("Hello, Swift World!")
-    print("Hello, Swift!!!")
     print("Goodbye......!")
-    print("Goodbye, World!")
-    print("End")
-    print("======= END ========")
 }
 
 main()
