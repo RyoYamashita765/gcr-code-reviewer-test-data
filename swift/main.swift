fe27bf354eaf4a9961d28e493e54673c18dd16ed
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 14:56:36 +0900

    Add one line in "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -3,6 +3,7 @@ import Foundation
 func main(){
     let arguments = CommandLine.arguments
     if arguments.count >= 1 {
+        print("Arguments count: \(arguments.count)")
         print("Hello, \(arguments[1])!")
     } else {
         print("Hello, World!")
