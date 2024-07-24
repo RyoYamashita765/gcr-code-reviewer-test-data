436c4b982f97187db626db711a511f8d15966bef
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:36:41 +0900

    Update one line in "while" block (1)

--- swift/main.swift
+++ swift/main.swift
@@ -6,7 +6,7 @@ func main(){
         print("a = \(a)")
         print("  a + 1 = \(a + 1)")
         print("  a + 2 = \(a + 2)")
-        print("  a + 3 = \(a + 3)")
+        print("  a + 4 = \(a + 4)")
         print("  a + a = \(a + a)")
         print("  a + a + a = \(a + a + a)")
         print("  a * a = \(a * a)")
