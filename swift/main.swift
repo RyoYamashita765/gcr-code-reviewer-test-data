ae5b9c4b24f61b77c0395272ea8326b82bb50b30
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:33:31 +0900

    Add one line to some points in "while" block

--- swift/main.swift
+++ swift/main.swift
@@ -4,10 +4,12 @@ func main(){
     var a = 1
     while a < 10 {
         print("a = \(a)")
+        print("  a + 1 = \(a + 1)")
         print("  a + a = \(a + a)")
         print("  a + a + a = \(a + a + a)")
         print("  a * a = \(a * a)")
         print("  a * a * a = \(a * a * a)")
+        print("")
         a += 1
     }
 }
