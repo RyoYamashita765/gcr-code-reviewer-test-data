ed6c5b3602a514950ed021389d98737b087182b5
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 17:36:54 +0900

    Update one line in "while" block (2)

--- swift/main.swift
+++ swift/main.swift
@@ -2,7 +2,7 @@ import Foundation
 
 func main(){
     var a = 1
-    while a < 10 {
+    while a <= 10 {
         print("a = \(a)")
         print("  a + 1 = \(a + 1)")
         print("  a + 2 = \(a + 2)")
