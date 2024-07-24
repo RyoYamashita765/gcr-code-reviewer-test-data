49ed4798d65d56138ed1a8834382578eb9dbf5a1
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 15:13:11 +0900

    Delete "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -2,15 +2,6 @@ import Foundation
 
 func main(){
     let arguments = CommandLine.arguments
-    if arguments.count > 0 {
-        print("Argument's count: \(arguments.count)")
-        print("Hello, \(arguments[1])!")
-    } else {
-        print("=== No Arguments ===")
-        print("Hello, Swift World!")
-        print("Lorem ipsum dolor sit amet,")
-        print("consectetur adipiscing elit.")
-    }
 }
 
 main()
