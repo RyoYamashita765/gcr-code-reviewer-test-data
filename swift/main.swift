e33417e90749518e44bdf1f9ba935508d9e403c6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 14:33:04 +0900

    Add multiple lines to some points

--- swift/main.swift
+++ swift/main.swift
@@ -1,11 +1,15 @@
 import Foundation
 
 func main(){
+    print("Programming Test")
+    print("By Swift language")
     print("--------------------")
     print("Hello, World!")
     print("Hello, Swift!")
     print("Goodbye, Swift!")
     print("Goodbye, World!")
+    print("--------------------")
+    print("End of Programming Test")
     print("--------------------")
 }
 
