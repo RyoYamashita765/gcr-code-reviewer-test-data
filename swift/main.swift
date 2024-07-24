dc1a28565e6a20852ccf52e5b5e7b050911663eb
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 14:32:48 +0900

    Add one line to some points

--- swift/main.swift
+++ swift/main.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 func main(){
+    print("--------------------")
     print("Hello, World!")
     print("Hello, Swift!")
     print("Goodbye, Swift!")
     print("Goodbye, World!")
+    print("--------------------")
 }
 
 main()
