18b45a2f022a2ec472fc01f1184645df583a7239
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-24 15:12:54 +0900

    Delete one line from some points in "if" block

--- swift/main.swift
+++ swift/main.swift
@@ -3,7 +3,6 @@ import Foundation
 func main(){
     let arguments = CommandLine.arguments
     if arguments.count > 0 {
-        print("--- Command Line Arguments ---")
         print("Argument's count: \(arguments.count)")
         print("Hello, \(arguments[1])!")
     } else {
@@ -11,7 +10,6 @@ func main(){
         print("Hello, Swift World!")
         print("Lorem ipsum dolor sit amet,")
         print("consectetur adipiscing elit.")
-        print("======================")
     }
 }
 
