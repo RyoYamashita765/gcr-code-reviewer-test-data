dd6debfefa8dd6a683b1e4dedb8719527b5fcce6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:22:28 +0900

    Add "if" block

--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        println!("You passed {} arguments", args.len() - 1);
+    } else {
+        println!("You passed no arguments");
+    }
 }
