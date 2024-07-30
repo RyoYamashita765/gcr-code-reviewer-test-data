a5c9b3779148f6d353fb581ee5b480572b10475d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:18:49 +0900

    Update lines

--- rust/main.rs
+++ rust/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!("-----------------");
+    println!("=================");
     println!("Hello, world!");
     println!("Goodbye, world!");
-    println!("-----------------");
+    println!("=================");
 }
