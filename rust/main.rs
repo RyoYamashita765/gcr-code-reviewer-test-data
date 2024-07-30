105cb2be5d9fb1f721ab840b59707ee7cba4fb04
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:59:19 +0900

    Delete "match" block

--- rust/main.rs
+++ rust/main.rs
@@ -1,15 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    let args: Vec<String> = std::env::args().collect();
-    match args.len() {
-        1 => {
-            println!("No arguments provided");
-        },
-        2 => {
-            println!("One argument provided");
-        },
-        _ => {
-            println!("More than one argument provided");
-        },
-    }
 }
