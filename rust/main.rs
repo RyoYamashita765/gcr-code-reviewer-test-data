6d5047ddb83052c10b7b9ffe0170462ee99edfba
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 19:00:41 +0900

    Add sample case: "MUST avoid hard-coding."

--- rust/main.rs
+++ rust/main.rs
@@ -1,21 +1,5 @@
-struct Person {
-    name: String,
-    age: u32,
-}
-
-impl Person {
-    fn new(name: String, age: u32) -> Self {
-        Person { name, age }
-    }
-
-    fn say_hello_method(&self) {
-        println!("Hello, {}.", self.name);
-        println!("  {} years old.", self.age);
-    }
-}
-
 fn main() {
-    let person = Person::new(String::from("John"), 30);
-    println!("Person: {}, {}", person.name, person.age);
-    person.say_hello_method();
+    let radius = 5.0;
+    let area = 3.141592 * radius * radius;
+    println!("The area of a circle with radius {} is {}", radius, area);
 }
