447647210acb3f51a6508cbdc6902ebbb6dbcc70
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:23:21 +0900

    Add one line in "if" block

--- rust/main.rs
+++ rust/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         println!("You passed {} arguments", args.len() - 1);
+        println!("args: {:?}", args);
     } else {
         println!("You passed no arguments");
     }
