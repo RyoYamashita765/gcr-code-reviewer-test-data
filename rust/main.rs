4819470cd8cdc78d93dd369955d6c4dbd83009ce
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 15:12:19 +0900

    Update lines in "while" block

--- rust/main.rs
+++ rust/main.rs
@@ -2,9 +2,9 @@ fn main() {
     let mut a = 0;
     while a < 20 {
         println!("Hello, world!");
-        a += 1;
+        a *= 2;
         a -= 1;
-        a += 1;
+        a += 2;
         println!("---");
     }
 }
