e126d6e3ef6af1c3fb221fcfc461484fbfd39ced
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:53:12 +0900

    Add "match" block

--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        1 => {
+            println!("No arguments provided");
+        },
+        2 => {
+            println!("One argument provided");
+        },
+        _ => {
+            println!("More than one argument provided");
+        },
+    }
 }
