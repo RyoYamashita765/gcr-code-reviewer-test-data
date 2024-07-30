1caec75da27123e62e8a100704d3f3c67a7d0f87
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:27:53 +0900

    Delete "if" block

--- rust/main.rs
+++ rust/main.rs
@@ -1,9 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() >= 2 {
-        println!("You passed {} arguments", args.len() - 1);
-    } else {
-        println!("You passed no arguments");
-    }
 }
