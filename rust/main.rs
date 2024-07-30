b07a7612a8a592d725f6b918f2e40b08b5ac7957
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 17:10:03 +0900

    Add impl from struct and trait

--- rust/main.rs
+++ rust/main.rs
@@ -13,8 +13,35 @@ impl Person {
     }
 }
 
+trait Logger {
+    fn info(&self, message: &str);
+    fn warning(&self, message: &str);
+    fn error(&self, message: &str);
+}
+
+struct ConsoleLogger;
+
+impl Logger for ConsoleLogger {
+    fn info(&self, message: &str) {
+        println!("INFO: {}", message);
+    }
+
+    fn warning(&self, message: &str) {
+        println!("WARNING: {}", message);
+    }
+
+    fn error(&self, message: &str) {
+        println!("ERROR: {}", message);
+    }
+}
+
 fn main() {
     let person = Person::new(String::from("John"), 30);
     println!("Person: {}, {}", person.name, person.age);
     person.say_hello();
+
+    let logger = ConsoleLogger;
+    logger.info("This is an info message.");
+    logger.warning("This is a warning message.");
+    logger.error("This is an error message.");
 }
