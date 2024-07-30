3b8a36762f1df52eb91978622398ce5f33f46c89
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 14:36:32 +0900

    Update lines in "for" block

--- rust/main.rs
+++ rust/main.rs
@@ -2,9 +2,9 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = std::env::args().collect();
     for i in 1..args.len() {
-        println!("  argument: {}", args[i]);
+        println!("  {}", args[i]);
         println!("---");
         println!("///");
-        println!("+++");
+        println!("   ");
     }
 }
