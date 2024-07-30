99b6a622c867ed79266c16ee87185e5fc2f1c1cb
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 17:00:26 +0900

    Add fn in impl

--- rust/main.rs
+++ rust/main.rs
@@ -7,9 +7,14 @@ impl Person {
     fn new(name: String, age: u32) -> Self {
         Person { name, age }
     }
+
+    fn say_hello(&self) {
+        println!("Hello, my name is {}.", self.name);
+    }
 }
 
 fn main() {
     let person = Person::new(String::from("John"), 30);
     println!("Person: {}, {}", person.name, person.age);
+    person.say_hello();
 }
