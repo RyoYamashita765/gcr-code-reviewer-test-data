bfe0012aab0c4e1c2cfc9426c0e54495f76a3e51
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 18:14:00 +0900

    Delete impl and struct

--- rust/main.rs
+++ rust/main.rs
@@ -14,25 +14,8 @@ impl Person {
     }
 }
 
-struct ConsoleLogger;
-
-impl ConsoleLogger {
-    fn info(&self, message: &str) {
-        println!("INFO: {}", message);
-    }
-
-    fn warn(&self, message: &str) {
-        println!("WARNING: {}", message);
-        println!("  Please proceed with caution.");
-    }
-}
-
 fn main() {
     let person = Person::new(String::from("John"), 30);
     println!("Person: {}, {}", person.name, person.age);
     person.say_hello_method();
-
-    let logger = ConsoleLogger;
-    logger.info("This is an info message.");
-    logger.warn("This is a warning message.");
 }
