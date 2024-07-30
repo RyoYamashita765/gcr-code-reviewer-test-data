9fb16c85137975fd201dc7f1dbc535203e04f924
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-30 15:23:41 +0900

    Add impl from struct

--- rust/main.rs
+++ rust/main.rs
@@ -1,2 +1,15 @@
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    fn new(name: String, age: u32) -> Self {
+        Person { name, age }
+    }
+}
+
 fn main() {
+    let person = Person::new(String::from("John"), 30);
+    println!("Person: {}, {}", person.name, person.age);
 }
