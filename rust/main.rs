4345d0a711611309c6e3f7950f72aba6c178d827
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-31 12:23:03 +0900

    Add sample case: "Define an interface when introducing a new class."

--- rust/main.rs
+++ rust/main.rs
@@ -1,5 +1,25 @@
+struct Car {
+    brand: String,
+    model: String,
+    year: u32,
+}
+
+impl Car {
+    fn new(brand: String, model: String, year: u32) -> Self {
+        Car { brand, model, year }
+    }
+
+    fn start_engine(&self) {
+        println!("The {} {} engine is starting...", self.brand, self.model);
+    }
+
+    fn stop_engine(&self) {
+        println!("The {} {} engine is stopping...", self.brand, self.model);
+    }
+}
+
 fn main() {
-    let radius = 5.0;
-    let area = 3.141592 * radius * radius;
-    println!("The area of a circle with radius {} is {}", radius, area);
+    let my_car = Car::new(String::from("FooBar"), String::from("HogePiyo"), 2022);
+    my_car.start_engine();
+    my_car.stop_engine();
 }
