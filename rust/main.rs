71af56796f9a4f83635a9f3f46f438c592cae642
Ryo Yamashita <yamashita@ipride.co.jp>
2024-07-31 12:24:26 +0900

    Add sample case: "Do NOT use inheritance as possible, use composition instead."

--- rust/main.rs
+++ rust/main.rs
@@ -1,25 +1,63 @@
-struct Car {
-    brand: String,
-    model: String,
-    year: u32,
+trait Animal {
+    fn new(name: String) -> Self;
+    fn name(&self) -> &str;
+    fn make_sound(&self);
 }
 
-impl Car {
-    fn new(brand: String, model: String, year: u32) -> Self {
-        Car { brand, model, year }
+struct Dog {
+    name: String,
+}
+
+impl Animal for Dog {
+    fn new(name: String) -> Self {
+        Dog { name }
     }
 
-    fn start_engine(&self) {
-        println!("The {} {} engine is starting...", self.brand, self.model);
+    fn name(&self) -> &str {
+        &self.name
     }
 
-    fn stop_engine(&self) {
-        println!("The {} {} engine is stopping...", self.brand, self.model);
+    fn make_sound(&self) {
+        println!("Woof!");
+    }
+}
+
+impl Dog {
+    fn fetch(&self) {
+        println!("{} is fetching.", self.name);
+    }
+}
+
+struct Cat {
+    name: String,
+}
+
+impl Animal for Cat {
+    fn new(name: String) -> Self {
+        Cat { name }
+    }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn make_sound(&self) {
+        println!("Meow!");
+    }
+}
+
+impl Cat {
+    fn scratch(&self) {
+        println!("{} is scratching.", self.name);
     }
 }
 
 fn main() {
-    let my_car = Car::new(String::from("FooBar"), String::from("HogePiyo"), 2022);
-    my_car.start_engine();
-    my_car.stop_engine();
+    let my_dog = Dog::new(String::from("Buddy"));
+    my_dog.make_sound();
+    my_dog.fetch();
+
+    let my_cat = Cat::new(String::from("Whiskers"));
+    my_cat.make_sound();
+    my_cat.scratch();
 }
