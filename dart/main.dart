d8e371dc77017124e7f6d91a94d8a6388b2d02a4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 18:10:00 +0900

    Update method's name

--- dart/main.dart
+++ dart/main.dart
@@ -3,7 +3,7 @@ class Person {
   int age;
   Person(this.name, this.age);
 
-  void sayHello() {
+  void greet() {
     print('Hello, I am $name.');
     print('I am $age years old.');
     print('\n');
@@ -35,7 +35,7 @@ class Dog implements Animal {
 void main() {
   final person = Person('Alice', 30);
   print('Name: ${person.name}, Age: ${person.age}');
-  person.sayHello();
+  person.greet();
 
   final dog = Dog();
   dog.makeSound();
