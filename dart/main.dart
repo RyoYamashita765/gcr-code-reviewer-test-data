3c18bf0f0ac2d7e38b288b2078fbd2b6a3c2c272
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 18:06:02 +0900

    Add abstract class and class

--- dart/main.dart
+++ dart/main.dart
@@ -8,8 +8,21 @@ class Person {
   }
 }
 
+abstract class Animal {
+  void makeSound();
+}
+
+class Dog implements Animal {
+  void makeSound() {
+    print('Bark!');
+  }
+}
+
 void main() {
   final person = Person('Alice', 30);
   print('Name: ${person.name}, Age: ${person.age}');
   person.sayHello();
+
+  final dog = Dog();
+  dog.makeSound();
 }
