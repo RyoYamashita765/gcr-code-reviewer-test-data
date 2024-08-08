ef50e4c98c5d66d6bc9b4b43defeca3c79856677
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:43:46 +0900

    Update lines in "for" block

--- dart/main.dart
+++ dart/main.dart
@@ -2,9 +2,9 @@ void main() {
   const pets = ['cat', 'dog', 'fish'];
 
   for (var i = pets.length - 1; i >= 0; i--) {
-    print('Pet $i: ');
+    print('Pet $i:  ');
     print(pets[i]);
     print('---');
-    print('');
+    print('---');
   }
 }
