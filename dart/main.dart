cc37341361bc69a95cf8252380ed1dc52dac5869
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 18:04:18 +0900

    Add method in class

--- dart/main.dart
+++ dart/main.dart
@@ -2,9 +2,14 @@ class Person {
   String name;
   int age;
   Person(this.name, this.age);
+
+  void sayHello() {
+    print('Hello, my name is $name.');
+  }
 }
 
 void main() {
   final person = Person('Alice', 30);
   print('Name: ${person.name}, Age: ${person.age}');
+  person.sayHello();
 }
