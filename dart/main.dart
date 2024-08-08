5685c7f08831a9c845c0abd8b0b5be247e62a6fa
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:46:44 +0900

    Add "switch" block

--- dart/main.dart
+++ dart/main.dart
@@ -1,3 +1,17 @@
 void main() {
   const pets = ['cat', 'dog', 'fish'];
+
+  switch (pets[0]) {
+    case 'cat':
+      print('meow!');
+      break;
+    case 'dog':
+      print('woof!');
+      break;
+    case 'fish':
+      print('...');
+      break;
+    default:
+      print('(unknown)');
+  }
 }
