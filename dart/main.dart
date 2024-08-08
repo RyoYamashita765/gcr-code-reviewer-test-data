9b6b23b19be50d6fd71268d6de1fd17ec2f83880
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 18:00:17 +0900

    Delete lines in "while" block

--- dart/main.dart
+++ dart/main.dart
@@ -2,9 +2,7 @@ void main() {
   var pets = ['cat', 'dog', 'fish'];
 
   while (pets.length > 0) {
-    print('====================');
     print('I have ${pets}.');
     pets.remove(pets.last);
-    print('Now I have ${pets.length} pets');
   }
 }
