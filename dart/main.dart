d1d6532ce7e6a3ae6f1a44d8f915598f19001fac
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:42:30 +0900

    Add lines in "for" block

--- dart/main.dart
+++ dart/main.dart
@@ -2,7 +2,9 @@ void main() {
   const pets = ['cat', 'dog', 'fish'];
 
   for (var i = 0; i < pets.length; i++) {
+    print('Pet $i: ');
     print(pets[i]);
+    print('');
     print('');
   }
 }
