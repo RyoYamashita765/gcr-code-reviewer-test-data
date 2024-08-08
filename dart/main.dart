04d6dfbd30060e9859ec0461d7957c3db7b56936
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:53:52 +0900

    Update lines in "switch" block

--- dart/main.dart
+++ dart/main.dart
@@ -4,11 +4,11 @@ void main() {
   switch (pets[0]) {
     case 'cat':
       print('meow!');
-      print('purr');
+      print('meow!');
       break;
     case 'dog':
       print('woof!');
-      print('bow wow');
+      print('woof!');
       break;
     case 'fish':
       print('...');
