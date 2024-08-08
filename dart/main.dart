575d1b9e106f8a7a5cb3d2bf59866e7e2efeb474
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:58:07 +0900

    Add lines in "while" block

--- dart/main.dart
+++ dart/main.dart
@@ -2,8 +2,10 @@ void main() {
   var pets = ['cat', 'dog', 'fish'];
 
   while (pets.isNotEmpty) {
+    print('-----------------');
     print('I have a ${pets.last}');
     pets.removeLast();
     print('Now I have ${pets.length} pets');
+    print('');
   }
 }
