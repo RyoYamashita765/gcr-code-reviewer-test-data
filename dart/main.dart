a5bdf8ae324578f4ba00c12f74707d779c65a431
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:40:17 +0900

    Delete "if" block

--- dart/main.dart
+++ dart/main.dart
@@ -1,9 +1,3 @@
 void main() {
   const pets = ['cat', 'dog', 'fish'];
-
-  if (pets[0] == 'cat') {
-    print('The pets list contains a cat.');
-  } else {
-    print('The pets list does not contain a cat.');
-  }
 }
