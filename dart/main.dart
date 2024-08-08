2811cdbf740e252fc24b6b9175b17d1d4d1646e4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:54:37 +0900

    Delete "switch" block

--- dart/main.dart
+++ dart/main.dart
@@ -1,17 +1,3 @@
 void main() {
   const pets = ['cat', 'dog', 'fish'];
-
-  switch (pets[0]) {
-    case 'cat':
-      print('meow!');
-      break;
-    case 'dog':
-      print('woof!');
-      break;
-    case 'fish':
-      print('...');
-      break;
-    default:
-      print('(unknown)');
-  }
 }
