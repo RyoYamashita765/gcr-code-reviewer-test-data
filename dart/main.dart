f228720de4617cc686e2a843f431d762d97c5f08
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-08 17:37:23 +0900

    Add lines in "if" block

--- dart/main.dart
+++ dart/main.dart
@@ -2,7 +2,9 @@ void main() {
   const pets = ['cat', 'dog', 'fish'];
 
   if (pets.contains('cat')) {
+    print('---------------------------------');
     print('The pets list contains a cat.');
+    print('---------------------------------');
   } else {
     print('The pets list does not contain a cat.');
     print('The pets list contains: $pets');
